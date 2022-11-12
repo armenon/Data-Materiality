bbb030e366763122288b9db9a285a0d8e02aa3c3
Arjun Menon <ar.menon28@gmail.com>
2022-11-12 11:40:10 +0100

    Add files via upload
    New feedback mechanisms and class based structures

--- estimation_module/Constants.h
+++ estimation_module/Constants.h
@@ -8,12 +8,14 @@
 #define DOUT3 8
 #define DOUT4 9
 #define CLK 0
-#define NEOPIN1 5
-#define NEOPIN2 4
-#define NEOPIN3 3
-#define NEOPIN4 2
+// #define NEOPIN1 5
+// #define NEOPIN2 4
+// #define NEOPIN3 3
+// #define NEOPIN4 2
 #define NUMPIXELS 14
 #define NUMOFPLATFORMS 4
 #define NUMOFFOODS 1
 #define BUTTONPIN 1
+
+#define MAXLEDCOUNT 16
 
